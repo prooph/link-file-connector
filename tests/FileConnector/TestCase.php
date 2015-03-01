2a2755835f9465541ab13bf195ac499de7250629
Alexander Miertsch <alexander.miertsch@sixt.com>
2015-03-01 22:51:58 +0100

    Move tests from link

---
+++ tests/FileConnector/TestCase.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* This file is part of prooph/link.
+ * (c) prooph software GmbH <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 03.01.15 - 22:03
+ */
+
+namespace ProophTest\Link\FileConnector;
+
+/**
+ * Class TestCase
+ *
+ * @package FileConnectorTest
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return string
+     */
+    protected function getTestDataPath()
+    {
+        return __DIR__ . '/data/';
+    }
+}
+ 
